88d46ff616458871eb6ed01a8f02aa7d30dd8829
Udaya <udaya.kahandawala@spemai.com>
2022-07-12 16:12:31 +0530

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by amilla fernando on 2022-07-12.
+//
+
+import Foundation
+
+public struct SDK{
+    
+    static func doSomeWork(){
+        
+        print("do some work...")
+    }
+}
